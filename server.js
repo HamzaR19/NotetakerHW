a3ee1b79f18b7f5e9c6716e1ab93615b8f04d252
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-25 14:27:38 -0500

    added things

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const app = express();
 const notes = require('./db/db.json');
@@ -6,10 +7,34 @@ const path = require('path');
 const uuid = require('./helpers/uuid');
 
 app.use (express.json());
-app.use ('/api/notes', require('./api'));
+// app.use ('/api/notes', require('./api'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3001;
+app.get('/' , (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
