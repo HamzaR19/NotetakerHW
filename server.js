da424ce4d37a31b50ebfd7308ac03aa005121de4
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-25 14:32:57 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -16,6 +16,37 @@ app.get('/' , (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes' , (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+}
+);
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+}
+);
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuid();
+    notes.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+}   
+);
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    notes.splice(noteId, 1);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+},
+);
+
+
+
 
 
 
